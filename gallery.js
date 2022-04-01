ee5cea940b67cfc2b474349c7e29668ee81396ab
Caleb Barzee <barzeec@gmail.com>
2022-04-01 10:53:33 -0600

    data object update

--- gallery.js
+++ gallery.js
@@ -1,7 +1,160 @@
-art = {
-    "id": 0001,
+function main() {
+const art = {
+    "0000": {
+    "id": "0000",
+	"movement": "Abstract Expressionism",
     "title": "Recumbent Figure",
     "artist": "Henry Moore",
     "date": "1938",
     "filepath": "assets/8-bit_art/abstract_expressionism/pixelated_recumbent_figure.jpeg"
+    },
+    "0001": {
+        "id": "0001",
+	    "movement": "Baroque",
+        "title": "The Calling of St. Matthew",
+        "artist": "Caravaggio",
+        "date": "1600",
+        "filepath": "assets/8-bit_art/baroque/pixelated_calling_of_st_matthew.jpeg"
+        },
+    "0002": {
+        "id": "0002",
+	    "movement": "Baroque",
+        "title": "Judith Beheading Holofernes",
+        "artist": "Artemisia Gentileschi",
+        "date": "1613",
+        "filepath": "assets/8-bit_art/baroque/pixelated_judith_beheading_holofernes.jpeg"
+        },
+    "0003": {
+        "id": "0003",
+	    "movement": "Baroque",
+        "title": "Las Meninas",
+        "artist": "Diego Velázquez",
+        "date": "1656",
+        "filepath": "assets/8-bit_art/baroque/pixelated_las_meninas.jpeg"
+        },
+    "0004": {
+        "id": "0004",
+	    "movement": "Baroque",
+        "title": "The Night Watch",
+        "artist": "Rembrandt van Rijn",
+        "date": "1642",
+        "filepath": "assets/8-bit_art/baroque/pixelated_the_night_watch.jpeg"
+        },
+    "0005": {
+        "id": "0005",
+	    "movement": "Cubism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/cubism/"
+    },
+    "0006": {
+        "id": "0006",
+	    "movement": "Cubism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/cubism/"
+        },
+    "0007": {
+        "id": "0007",
+	    "movement": "Cubism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/cubism/"
+        },
+    "0008": {
+        "id": "0008",
+	    "movement": "Cubism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/cubism/"
+        },
+    "0009": {
+        "id": "0009",
+	    "movement": "Cubism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/cubism/"
+        },
+    "0010": {
+        "id": "0010",
+	    "movement": "Cubism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/cubism/"
+        },
+    "0011": {
+        "id": "0011",
+	    "movement": "Dada",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/dada/"
+        },
+    "0012": {
+        "id": "0012",
+	    "movement": "Dada",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/dada/"
+        },
+    "0013": {
+        "id": "0013",
+	    "movement": "Dada",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/dada/"
+        },
+    "0014": {
+        "id": "0014",
+	    "movement": "Dada",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/dada/"
+        },
+    "0015": {
+        "id": "0015",
+	    "movement": "Expressionism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/expressionism/"
+        },
+    "0016": {
+        "id": "0016",
+	    "movement": "Expressionism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/expressionism/"
+        },
+    "0017": {
+        "id": "0017",
+	    "movement": "Expressionism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/expressionism/"
+        },
+    "0018": {
+        "id": "0018",
+	    "movement": "Expressionism",
+        "title": "",
+        "artist": "",
+        "date": "",
+        "filepath": "assets/8-bit_art/expressionism/"
+        },
+
+};
+
+console.log(art);
 }
+main();

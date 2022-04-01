ba9127414d17bb4e9ad639ea59d47679f0a84df1
Caleb Barzee <barzeec@gmail.com>
2022-04-01 12:33:09 -0600

    js object data update

--- gallery.js
+++ gallery.js
@@ -1,6 +1,5 @@
-function main() {
 const art = {
-    "0000": {
+    0: {
     "id": "0000",
 	"movement": "Abstract Expressionism",
     "title": "Recumbent Figure",
@@ -8,7 +7,7 @@ const art = {
     "date": "1938",
     "filepath": "assets/8-bit_art/abstract_expressionism/pixelated_recumbent_figure.jpeg"
     },
-    "0001": {
+    1: {
         "id": "0001",
 	    "movement": "Baroque",
         "title": "The Calling of St. Matthew",
@@ -16,7 +15,7 @@ const art = {
         "date": "1600",
         "filepath": "assets/8-bit_art/baroque/pixelated_calling_of_st_matthew.jpeg"
         },
-    "0002": {
+    2: {
         "id": "0002",
 	    "movement": "Baroque",
         "title": "Judith Beheading Holofernes",
@@ -24,7 +23,7 @@ const art = {
         "date": "1613",
         "filepath": "assets/8-bit_art/baroque/pixelated_judith_beheading_holofernes.jpeg"
         },
-    "0003": {
+    3: {
         "id": "0003",
 	    "movement": "Baroque",
         "title": "Las Meninas",
@@ -32,7 +31,7 @@ const art = {
         "date": "1656",
         "filepath": "assets/8-bit_art/baroque/pixelated_las_meninas.jpeg"
         },
-    "0004": {
+    4: {
         "id": "0004",
 	    "movement": "Baroque",
         "title": "The Night Watch",
@@ -40,140 +39,157 @@ const art = {
         "date": "1642",
         "filepath": "assets/8-bit_art/baroque/pixelated_the_night_watch.jpeg"
         },
-    "0005": {
+    5: {
         "id": "0005",
 	    "movement": "Cubism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/cubism/"
+        "title": "Guernica",
+        "artist": "Pablo Picasso",
+        "date": "1937",
+        "filepath": "assets/8-bit_art/cubism/pixelated_guernica.jpeg"
     },
-    "0006": {
+    6: {
         "id": "0006",
 	    "movement": "Cubism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/cubism/"
+        "title": "Guitar, Sheet Music, and Wine Glass",
+        "artist": "Pablo Picasso",
+        "date": "1912",
+        "filepath": "assets/8-bit_art/cubism/pixelated_guitar_sheet_music_and_glass.jpeg"
         },
-    "0007": {
+    7: {
         "id": "0007",
 	    "movement": "Cubism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/cubism/"
+        "title": "Nude Descending a Staircase",
+        "artist": "Marcel Duchamp",
+        "date": "1912",
+        "filepath": "assets/8-bit_art/cubism/pixelated_nude_descending_a_staircase.jpeg"
         },
-    "0008": {
+    8: {
         "id": "0008",
 	    "movement": "Cubism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/cubism/"
+        "title": "Quarry at Bibémus",
+        "artist": "Paul Cézanne",
+        "date": "1900",
+        "filepath": "assets/8-bit_art/cubism/pixelated_quarry_bibemus.jpeg"
         },
-    "0009": {
+    9: {
         "id": "0009",
 	    "movement": "Cubism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/cubism/"
+        "title": "Les Demoiselles d'Avignon",
+        "artist": "Pablo Picasso",
+        "date": "1907",
+        "filepath": "assets/8-bit_art/cubism/pixelated_the_damsels_of_d'avignon.jpeg"
         },
-    "0010": {
+    10: {
         "id": "0010",
 	    "movement": "Cubism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/cubism/"
+        "title": "Three Musicians",
+        "artist": "Pablo Picasso",
+        "date": "1921",
+        "filepath": "assets/8-bit_art/cubism/pixelated_the_three-musicians.jpeg"
         },
-    "0011": {
+    11: {
         "id": "0011",
 	    "movement": "Dada",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/dada/"
+        "title": "Bicycle Wheel",
+        "artist": "Marcel Duchamp",
+        "date": "1913",
+        "filepath": "assets/8-bit_art/dada/pixelated_bicycle_wheel.jpeg"
         },
-    "0012": {
+    12: {
         "id": "0012",
 	    "movement": "Dada",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/dada/"
+        "title": "Bottle Rack",
+        "artist": "Marcel Duchamp",
+        "date": "1914",
+        "filepath": "assets/8-bit_art/dada/pixelated_bottle_rack.jpeg"
         },
-    "0013": {
+    13: {
         "id": "0013",
 	    "movement": "Dada",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/dada/"
+        "title": "Fountain",
+        "artist": "Marcel Duchamp",
+        "date": "1917",
+        "filepath": "assets/8-bit_art/dada/pixelated_fountain.jpeg"
         },
-    "0014": {
+    14: {
         "id": "0014",
 	    "movement": "Dada",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/dada/"
+        "title": "Cut with the Dada Kitchen Knife through the Last Weimar Beer-Belly",
+        "artist": "Hannah Höch",
+        "date": "1919",
+        "filepath": "assets/8-bit_art/dada/pixelated_hoch_cut_with_the_kitchen_knife_dada.jpeg"
         },
-    "0015": {
+    15: {
         "id": "0015",
 	    "movement": "Expressionism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/expressionism/"
+        "title": "I and the Village",
+        "artist": "Marc Chagall",
+        "date": "1911",
+        "filepath": "assets/8-bit_art/expressionism/pixelated_i_and_the_village.jpg"
         },
-    "0016": {
+    16: {
         "id": "0016",
-	    "movement": "Expressionism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/expressionism/"
+	    "movement": "Symbolism",
+        "title": "Kiss by the Window",
+        "artist": "Edvard Munch",
+        "date": "1892",
+        "filepath": "assets/8-bit_art/expressionism/pixelated_kiss_by_the_window.jpeg"
         },
-    "0017": {
+    17: {
         "id": "0017",
 	    "movement": "Expressionism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/expressionism/"
+        "title": "Mount Niesen",
+        "artist": "Paul Klee",
+        "date": "1915",
+        "filepath": "assets/8-bit_art/expressionism/pixelated_mount_niesen.jpeg"
         },
-    "0018": {
+    18: {
         "id": "0018",
 	    "movement": "Expressionism",
-        "title": "",
-        "artist": "",
-        "date": "",
-        "filepath": "assets/8-bit_art/expressionism/"
+        "title": "Red Balloon",
+        "artist": "Paul Klee",
+        "date": "1922",
+        "filepath": "assets/8-bit_art/expressionism/pixelated_red_balloon.jpg"
         },
 
 };
-
 console.log(art);
-reset_view()
+function main() {
+var art_id = generate_id();
+reset_view(art_id);
 }
 
-check_form_submission(){
-    // checks response to see if it matches database
+function generate_id(){
+    let id = Math.floor(Math.random() * (19 - 0) + 0);
+    // max = 18, min = 0
+    console.log(id);
+    return id
+}
 
-    display_results();
+function check_form_submission(title_response, artist_response){
+    // checks response to see if it matches database
+    lowercase_title = art.art_id.title.toLowerCase();
+    lowercase_artist = art.art_id.artist.toLowerCase();
+    eval = lowercase_title == title_response.toLowerCase() && lowercase_artist == artist_response.toLowerCase();
+    display_results(eval);
     
 }
-display_results(){
+function display_results(eval){
     // displays if user is correct or not and the painting information
-
-    setTimeout(10000);
-    reset_view();
+    if (eval){
+        // user was correct
+        document.getElementById("result").innerHTML = "Correct!"
+    }
+    else {
+        // user was incorrect
+        document.getElementById("result").innerHTML = "Incorrect."
+    }
+    var art_id = generate_id();
+    setTimeout(reset_view(art_id), 10000);
 }
-reset_view(){
+function reset_view(art_id){
     // displays form and new art for user to guess again
-    Math.random()
+    document.getElementById("art_img").src = art.art_id.filepath
+    document.getElementById("art_img").alt = art.art_id.title
 }
-
 main();

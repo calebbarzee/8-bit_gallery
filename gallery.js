618579405720bdfe0e624902559d0f525d952cdd
Caleb Barzee <barzeec@gmail.com>
2022-03-31 23:28:03 -0600

    filepath fix

---
+++ gallery.js
@@ -0,0 +1,7 @@
+art = {
+    "id": 0001,
+    "title": "Recumbent Figure",
+    "artist": "Henry Moore",
+    "date": "1938",
+    "filepath": "assets/8-bit_art/abstract_expressionism/pixelated_recumbent_figure.jpeg"
+}
